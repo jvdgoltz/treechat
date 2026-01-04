0cef0d7eb516fa387c1bc2022c8dbb87a0b07cb1
Julian von der Goltz <hubi@PF31ZFYY.localdomain>
2026-01-04 11:01:08 +0100

    Post message endpoint

--- apps/api/app.py
+++ apps/api/app.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/chat")
+async def send_message(question: str):
+    return {"question": question, "answer": "Your question was " + question}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app:app", port=5000, log_level="info")
